56456fc62e0457b8d045b40a750f7c5807d5f0b3
Dongjae Won <bigdj2002@naver.com>
2023-04-10 01:56:23 +0900

    Code clean-up for main header

--- app/main.h
+++ app/main.h
@@ -1,17 +1,31 @@
+/** \headerfile for memory */
 #include "cmemory.h"
+
+/** \headerfile for STL */
 #include "cvector.h"
 #include "carray.h"
-#include "preprocessor.h"
-#include "clamda.h"
-#include "lrvalue.h"
-#include "cstring.h"
-#include "cpointer.h"
-#include "cinheritance.h"
 #include "cset.h"
 #include "cmap.h"
 #include "clist.h"
 #include "cstack.h"
 #include "cqueue.h"
+#include "cstring.h"
 
+/** \headerfile for preprocessor */
+#include "preprocessor.h"
+
+/** \headerfile for lamda function */
+#include "clamda.h"
+
+/** \headerfile for LR-value */
+#include "lrvalue.h"
+
+/** \headerfile for smart pointer */
+#include "cpointer.h"
+
+/** \headerfile for class inheritance */
+#include "cinheritance.h"
+
+/** \headerfile for thread */
 #include "cthread.h"
 #include "cthread_sync.h"
