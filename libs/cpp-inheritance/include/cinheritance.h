c7d2c8519b25b3b405b3cab03861febcb09e5965
Dongjae Won <bigdj2002@naver.com>
2023-04-16 02:06:36 +0900

    cpp inheritance

--- libs/cpp-inheritance/include/cinheritance.h
+++ libs/cpp-inheritance/include/cinheritance.h
@@ -1,3 +1,107 @@
 #include <iostream>
+#include <array>
+#include <random>
+
+/** \note: Reason to use inheritance
+ *    - Class relationship
+ *    - Code reuse
+ *    - Class interface consistency (abstract / interface, pure virtual function)
+ *    - Dynamic function binding (virtual function, virtual table)
+ */
 
 void inherance_example();
+
+class animal
+{
+public:
+  animal()
+  {
+    std::cout << "animal constructor" << std::endl;
+  }
+
+  /** \note: Base class destructor
+   *    - It shoud use virtual public and protected
+   *    - Because the destructor of leaf class is not called!
+   */
+  // ~animal()
+  virtual ~animal() = default;
+  // {
+  //   std::cout << "animal destructor" << std::endl;
+  // }
+
+  virtual void speak()
+  {
+    std::cout << "animal [pointing VT]" << std::endl;
+  }
+
+  virtual void speak2() = 0;
+
+  void sleep() const
+  {
+    std::cout << "I'm sleeping" << std::endl;
+  }
+
+private:
+  double height;
+};
+
+// class human : public animal
+class human
+{
+public:
+  void dirveCar() const
+  {
+    std::cout << "I'm driving" << std::endl;
+  }
+};
+
+class catInh : public animal
+{
+public:
+  catInh()
+  {
+    std::cout << "cat constructor" << std::endl;
+  }
+  ~catInh() = default;
+  // {
+  //   std::cout << "cat destructor" << std::endl;
+  // }
+
+  void speak() override
+  {
+    std::cout << "meow~ [pointing VT]" << std::endl;
+  }
+
+  void speak2() override
+  {
+    std::cout << "meow2~ [pointing VT]" << std::endl;
+  }
+
+private:
+  double weight;
+};
+
+class dogInh : public animal
+{
+public:
+  dogInh()
+  {
+    std::cout << "dog constructor" << std::endl;
+  }
+  ~dogInh() = default;
+  // {
+  //   std::cout << "dog destructor" << std::endl;
+  // }
+
+  void speak() override
+  {
+    std::cout << "bark!" << std::endl;
+  }
+
+  void speak2() override
+  {
+    std::cout << "bark2! [pointing VT]" << std::endl;
+  }
+};
+
+class
--- libs/cpp-inheritance/source/cinheritance.cpp
+++ libs/cpp-inheritance/source/cinheritance.cpp
@@ -2,5 +2,137 @@
 
 void inherance_example()
 {
-    
+  /* ---------------------------------------------------------------------------------------------------------- */
+
+  /**
+   * \brief: [1] class
+   *    - public ⊃ protected ⊃ private
+   * class A
+   * {
+   * public:
+   *   int x;
+   * protected:
+   *   int y;
+   * private:
+   *   int z;
+   * };
+   * class B : public A
+   * {
+   *   // x is public
+   *   // y is protected
+   *   // z is not accessible from B
+   * };
+   * class C : protected A
+   * {
+   *   // x is protected
+   *   // y is protected
+   *   // z is not accessible from C
+   * };
+   * class D : private A    // 'private' is default for classes
+   * {
+   *   // x is private
+   *   // y is private
+   *   // z is not accessible from D
+   * };
+   *    - IMPORTANT NOTE: Classes B, C and D all contain the variables x, y and z. It is just question of access.
+   */
+
+  human dongjae;
+  dongjae.dirveCar();
+  // dongjae.sleep();
+
+  std::cout << "------------------------------------- [↑ Example 1 ↑] -------------------------------------" << std::endl;
+
+  /* ---------------------------------------------------------------------------------------------------------- */
+
+  /**
+   * \brief: [2] virtual function
+   */
+
+  /** \example: #2-1 */
+  catInh cat;
+
+  /** \example: #2-2 */
+  // animal *animalPtr = new animal();
+  // delete animalPtr;
+
+  /** \example: #2-3 */
+  // catInh *catPtr = new catInh();
+  // delete catPtr;
+
+  /** \example: #2-4 */
+  // animal *polyCat = new catInh();
+  // delete polyCat;
+
+  /** \example: #2-5 (Dynamic or Runtime) polymorphism */
+  // animal *polyAnimal;
+  // polyAnimal = new catInh();
+  // polyAnimal->speak();
+  // delete polyAnimal;
+
+  // polyAnimal = new animal();
+  // polyAnimal->speak();
+  // delete polyAnimal;
+
+  /** \example: #2-6 (Dynamic or Runtime) polymorphism */
+  std::array<animal *, 5> animals; // It is better to use smart pointer
+  for (auto &animal : animals)
+  {
+    std::mt19937_64 rng(std::random_device{}());
+    std::uniform_int_distribution<int> dist(1, 2);
+    int random_int = dist(rng);
+    if (random_int == 1)
+      animal = new catInh();
+    else
+      animal = new dogInh();
+  }
+  for (auto &animal : animals)
+  {
+    animal->speak();
+    delete animal;
+  }
+
+  std::cout << "------------------------------------- [↑ Example 2 ↑] -------------------------------------" << std::endl;
+
+  /* ---------------------------------------------------------------------------------------------------------- */
+
+  /**
+   * \brief: [3] virtual table
+   */
+
+  /** \note: Added address memory of virtual function (Reason of additional 8bytes)
+   *         Pointer of the above address points virtual table of them
+   */
+  std::cout << "animal size: " << sizeof(animal) << std::endl;
+  std::cout << "cat size: " << sizeof(catInh) << std::endl;
+
+  animal *polyAnimal2 = new catInh;
+  polyAnimal2->speak();
+  delete polyAnimal2;
+
+  std::cout << "------------------------------------- [↑ Example 3 ↑] -------------------------------------" << std::endl;
+
+  /* ---------------------------------------------------------------------------------------------------------- */
+
+  /**
+   * \brief: [4] pure virtual table (For Abstract or Interface class)
+   *    - It cannot generate any object by itself
+   *    - Highly recommended to NOT use member variables in interface class (To be more flexible)
+   */
+
+  // animal abs; // compile ERROR!
+
+  std::cout << "------------------------------------- [↑ Example 4 ↑] -------------------------------------" << std::endl;
+
+  /* ---------------------------------------------------------------------------------------------------------- */
+
+  /**
+   * \brief: [5] Multiple inheritance
+   */
+
+  
+
+  std::cout << "------------------------------------- [↑ Example 5 ↑] -------------------------------------" << std::endl;
+
+  /* ---------------------------------------------------------------------------------------------------------- */
 }
