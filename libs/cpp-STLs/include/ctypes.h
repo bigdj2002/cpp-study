4b7fb844365344db8661fbd73a83319e3becbaff
Dongjae Won <bigdj2002@naver.com>
2023-04-17 10:16:47 +0900

    cpp types

--- libs/cpp-STLs/include/ctypes.h
+++ libs/cpp-STLs/include/ctypes.h
@@ -1,3 +1,28 @@
 #include <iostream>
 
+/** \remark: 1's complement
+ *
+ */
+
+/** \remark: 2's complement
+ *     \subpage: unsigned inteager
+ *               [0 0 0 0] → 0, 
+ *               [1 1 1 1] → 15
+ *     \subpage: signed inteager (MSB → for sign)
+ *               [0 0 0 0] → 0, 
+ *               [0 1 1 1] → 7, 
+ *               [1 0 0 0] → -8, 
+ *               [1 1 1 1] → -1
+ *     \subpage: float
+ *               15000 →       (+){1.5}  x 10^[4]
+ *               -380000 →     (-){3.8}  x 10^[5]
+ *               0.0000123 →   (+){1.23} x 10^[-5]
+ *               -0.0000123 →  (-){1.23} x 10^[-5]
+ *        - stucture: ( ) + { } + [ ] : Refer to IEEE 754 → [Sign] x [Mantissa] x 2^[Exponent]
+ *        - Sign 1bit + Exponent 8bit + Mantissa 23bit (Total 32bit float variable)
+ *        - Example) INF: (1) + {11111111} + [0000...000],
+ *                   NAN:     + {11111111} + [Not a zero],
+ *                   DENOM:   + {00000000} + [Not a zero]
+ */ 
+
 void types_example();
--- libs/cpp-STLs/source/ctypes.cpp
+++ libs/cpp-STLs/source/ctypes.cpp
@@ -2,5 +2,12 @@
 
 void types_example()
 {
+    const float num1 = 0.0f;
+    const float num2 = 0.4f;
+    const float sum = num1 + num2;
 
+    if (sum == 0.7f)
+        std::cout << "sum is 0.7f" << std::endl;
+    else
+        std::cout << "sum is not 0.7f" << std::endl;
 }
