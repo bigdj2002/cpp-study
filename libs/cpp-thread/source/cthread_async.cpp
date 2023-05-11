123571f75f2c98644caa23f35e07d69d7f92675b
Dongjae Won <bigdj2002@naver.com>
2023-05-12 01:30:57 +0900

    cpp thread async

--- libs/cpp-thread/source/cthread_async.cpp
+++ libs/cpp-thread/source/cthread_async.cpp
@@ -29,6 +29,33 @@ void sfut_fn(std::shared_future<int> fut)
   std::cout << "Num: " << fut.get() << std::endl;
 }
 
+int add1(int n)
+{
+  using namespace std::chrono_literals;
+  std::this_thread::sleep_for(2s);
+
+  throw std::runtime_error("Dongjae error");
+  return n + 1;
+}
+
+thread_local int localInt = 0;
+void async_fn1()
+{
+  std::cout << ++localInt << std::flush;
+}
+
+void task_1s()
+{
+  std::this_thread::sleep_for(1s);
+  std::cout << "task 1s" << std::endl;
+}
+
+void task_2s()
+{
+  std::this_thread::sleep_for(2s);
+  std::cout << "task 2s" << std::endl;
+}
+
 void thread_async_example()
 {
   /* ---------------------------------------------------------------------------------------------------------- */
@@ -85,7 +112,7 @@ void thread_async_example()
   std::shared_future<int> fut3 = prms3.get_future();
 
   std::vector<std::jthread> t3s;
-  for(int i=0; i<5; i++)
+  for (int i = 0; i < 5; i++)
   {
     t3s.emplace_back(sfut_fn, fut3);
   }
@@ -94,6 +121,55 @@ void thread_async_example()
   prms3.set_value(42);
 
   std::cout << "------------------------------------- [↑ Example 3 ↑] -------------------------------------" << std::endl;
+
+  /* ---------------------------------------------------------------------------------------------------------- */
+
+  /**
+   * \brief: [4] std::async
+   *    - The function template std::async runs the function f asynchronously
+   *      (potentially in a separate thread which might be a part of a thread pool)
+   *      and returns a std::future that will eventually hold the result of that function call.
+   *         1) Behaves as if (2) is called with policy being std::launch::async | std::launch::deferred.
+   *         2) Calls a function f with arguments args according to a specific launch policy policy (see below).
+   */
+
+  int num4 = 42;
+  try
+  {
+    std::future<int> fut4 = std::async(add1, num4); // Execution asynchronously! (Not execution in different thread)
+    int ret4 = fut4.get();
+    std::cout << "Ret: " << ret4 << std::endl;
+  }
+  catch (const std::exception &e)
+  {
+    std::cout << "Exception: " << e.what() << std::endl;
+  }
+
+  for (int i = 0; i < 20; i++)
+  {
+    auto fut_async_fn1 = std::async(std::launch::async, async_fn1);
+    // Is is better to consider callable object as a task!!
+  }
+
+  /** \details: std::future<T>::~future
+   *    - Releases any shared state. This means
+   *      * if the current object holds the last reference to its shared state, the shared state is destroyed;
+   *      * the current object gives up its reference to its shared state;
+   *    - These actions will not block for the shared state to become ready, except that they may block if all of the following are true:
+   *      1. the shared state was created by a call to std::async,
+   *      2. the shared state is not yet ready, and
+   *      3. the current object was the last reference to the shared state.
+   */
+
+  // Below codes take 2s (fire and forget)
+  auto fut4_a = std::async(std::launch::async, task_1s);
+  auto fut4_b = std::async(std::launch::async, task_2s);
+
+  // Below codes take 3s
+  // std::async(std::launch::async, task_1s);
+  // std::async(std::launch::async, task_2s);
+
+  std::cout << "------------------------------------- [↑ Example 4 ↑] -------------------------------------" << std::endl;
 
   /* ---------------------------------------------------------------------------------------------------------- */
 }
