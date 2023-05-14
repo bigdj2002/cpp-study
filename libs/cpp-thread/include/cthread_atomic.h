5c0522762a0f5e955b56a1b4ddf20430e0a19955
Dongjae Won <bigdj2002@naver.com>
2023-05-15 01:02:40 +0900

    cpp thread atomic

--- libs/cpp-thread/include/cthread_atomic.h
+++ libs/cpp-thread/include/cthread_atomic.h
@@ -4,18 +4,83 @@
 #include <vector>
 #include <mutex>
 #include <atomic>
+#include <array>
+#include <optional>
+#include <cassert>
 
 void thread_atomic_example();
 
 struct myStruct1
 {
-    int a;
-    int b;
+  int a;
+  int b;
 };
 
 struct myStruct2
 {
-    int a;
-    int b;
-    int c[100];
+  int a;
+  int b;
+  int c[100];
+};
+
+template <typename T, std::size_t N>
+class ThreadSafeStack
+{
+public:
+  ThreadSafeStack() : top_idx(-1) {}
+
+  bool push(const T &value)
+  {
+    int old_idx, new_idx;
+    do
+    {
+      old_idx = top_idx.load();
+      new_idx = old_idx + 1;
+      if (new_idx >= static_cast<int>(N))
+      {
+        return false; // Stack overflow
+      }
+    } while (!top_idx.compare_exchange_weak(old_idx, new_idx));
+
+    data[new_idx] = value;
+    return true;
+  }
+
+  std::optional<T> pop()
+  {
+    int old_idx, new_idx;
+    do
+    {
+      old_idx = top_idx.load();
+      if (old_idx < 0)
+      {
+        return std::nullopt; // Stack underflow
+      }
+      new_idx = old_idx - 1;
+    } while (!top_idx.compare_exchange_weak(old_idx, new_idx));
+
+    return data[old_idx];
+  }
+
+  std::optional<T> get(std::size_t index)
+  {
+    if (index >= N || index < 0)
+    {
+      return std::nullopt; // Invalid index
+    }
+    return data[index];
+  }
+
+  void print()
+  {
+    for (int i = 0; i <= top_idx.load(); ++i)
+    {
+      std::cout << data[i] << " ";
+    }
+    std::cout << std::endl;
+  }
+
+private:
+  std::array<T, N> data;
+  std::atomic<int> top_idx;
 };
--- libs/cpp-thread/source/cthread_atomic.cpp
+++ libs/cpp-thread/source/cthread_atomic.cpp
@@ -2,143 +2,207 @@
 
 void add_mutex(int &num, std::mutex &lck)
 {
-    for (int i = 0; i < 10000; i++)
-    {
-        const std::lock_guard<std::mutex> lock{lck};
-        ++num;
-    }
+  for (int i = 0; i < 10000; i++)
+  {
+    const std::lock_guard<std::mutex> lock{lck};
+    ++num;
+  }
 }
 
 void add_atomic(std::atomic<int> &num)
 {
-    for (int i = 0; i < 10000; i++)
+  for (int i = 0; i < 10000; i++)
+  {
+    ++num;
+  }
+}
+
+void push_func(ThreadSafeStack<int, 10> &stack, std::size_t stack_size)
+{
+  for (int i = 0; i < (int)stack_size / 2; ++i)
+  {
+    stack.push(i);
+    std::cout << "Pushed: " << i << ", Stack: ";
+    stack.print();
+    std::this_thread::sleep_for(std::chrono::milliseconds(10));
+  }
+}
+
+void pop_func(ThreadSafeStack<int, 10> &stack, std::size_t stack_size)
+{
+  for (int i = 0; i < (int)stack_size / 2; ++i)
+  {
+    auto result = stack.pop();
+    if (result.has_value())
     {
-        ++num;
+      std::cout << "Popped: " << result.value() << ", Stack: ";
+      stack.print();
     }
+    else
+    {
+      std::cout << "Stack underflow" << std::endl;
+    }
+    std::this_thread::sleep_for(std::chrono::milliseconds(10));
+  }
 }
 
 void thread_atomic_example()
 {
-    /* ---------------------------------------------------------------------------------------------------------- */
+  /* ---------------------------------------------------------------------------------------------------------- */
 
-    /**
-     * \brief: [1] atomic introduction
-     * \details:
-     */
+  /**
+   * \brief: [1] atomic introduction
+   * \details:
+   */
 
-    int num1_a = 0;
-    std::vector<std::thread> threads1_a, threads1_b;
-    std::mutex lock1; // May slow..
-    std::atomic<int> num1_b{0};
+  int num1_a = 0;
+  std::vector<std::thread> threads1_a, threads1_b;
+  std::mutex lock1; // May slow..
+  std::atomic<int> num1_b{0};
 
-    auto start1 = std::chrono::high_resolution_clock::now();
-    for (int i = 0; i < 4; i++)
-    {
-        threads1_a.emplace_back(add_mutex, std::ref(num1_a), std::ref(lock1));
-        ;
-    }
+  auto start1 = std::chrono::high_resolution_clock::now();
+  for (int i = 0; i < 4; i++)
+  {
+    threads1_a.emplace_back(add_mutex, std::ref(num1_a), std::ref(lock1));
+    ;
+  }
 
-    for (auto &thread : threads1_a)
-    {
-        thread.join();
-    }
+  for (auto &thread : threads1_a)
+  {
+    thread.join();
+  }
 
-    std::cout << num1_a << std::endl;
-    auto end1 = std::chrono::high_resolution_clock::now();
-    std::chrono::duration<double> time1 = end1 - start1;
-    std::cout << "Mutex based thread: " << time1.count() * 1000 << std::endl;
+  std::cout << num1_a << std::endl;
+  auto end1 = std::chrono::high_resolution_clock::now();
+  std::chrono::duration<double> time1 = end1 - start1;
+  std::cout << "Mutex based thread: " << time1.count() * 1000 << std::endl;
 
-    auto start2 = std::chrono::high_resolution_clock::now();
-    for (int i = 0; i < 4; i++)
-    {
-        threads1_b.emplace_back(add_atomic, std::ref(num1_b));
-    }
+  auto start2 = std::chrono::high_resolution_clock::now();
+  for (int i = 0; i < 4; i++)
+  {
+    threads1_b.emplace_back(add_atomic, std::ref(num1_b));
+  }
 
-    for (auto &thread : threads1_b)
-    {
-        thread.join();
-    }
+  for (auto &thread : threads1_b)
+  {
+    thread.join();
+  }
 
-    std::cout << num1_b << std::endl;
-    auto end2 = std::chrono::high_resolution_clock::now();
-    std::chrono::duration<double> time2 = end2 - start2;
-    std::cout << "Atomic based thread: " << time2.count() * 1000 << std::endl;
+  std::cout << num1_b << std::endl;
+  auto end2 = std::chrono::high_resolution_clock::now();
+  std::chrono::duration<double> time2 = end2 - start2;
+  std::cout << "Atomic based thread: " << time2.count() * 1000 << std::endl;
 
-    std::cout << "------------------------------------- [↑ Example 1 ↑] -------------------------------------" << std::endl;
+  std::cout << "------------------------------------- [↑ Example 1 ↑] -------------------------------------" << std::endl;
 
-    /* ---------------------------------------------------------------------------------------------------------- */
+  /* ---------------------------------------------------------------------------------------------------------- */
 
-    /**
-     * \brief: [2-1] methods of std::atomic
-     * \note: Maybe a lock free system depend on compilers (cfg. std::atomic_flag: 100% lock free)
-     * \details:
-     *    - Each instantiation and full specialization of the std::atomic template defines an atomic type.
-     *    - If one thread writes to an atomic object while another thread reads from it, the behavior is well-defined
-     *      (see memory model for details on data races).
-     *    - In addition, accesses to atomic objects may establish inter-thread synchronization and order non-atomic memory accesses as specified by std::memory_order.
-     *    - std::atomic is neither copyable nor movable.
-     *
-     * \brief: [2-2] std::atomic_flag
-     * \details:
-     *    - std::atomic_flag is an atomic boolean type.
-     *    - Unlike all specializations of std::atomic, it is guaranteed to be lock-free. (Check using is_lock_free())
-     *    - Unlike std::atomic<bool>, std::atomic_flag does not provide load or store operations.
-     */
+  /**
+   * \brief: [2-1] methods of std::atomic
+   * \note: Maybe a lock free system depend on compilers (cfg. std::atomic_flag: 100% lock free)
+   * \details:
+   *    - Each instantiation and full specialization of the std::atomic template defines an atomic type.
+   *    - If one thread writes to an atomic object while another thread reads from it, the behavior is well-defined
+   *      (see memory model for details on data races).
+   *    - In addition, accesses to atomic objects may establish inter-thread synchronization and order non-atomic memory accesses as specified by std::memory_order.
+   *    - std::atomic is neither copyable nor movable.
+   *
+   * \brief: [2-2] std::atomic_flag
+   * \details:
+   *    - std::atomic_flag is an atomic boolean type.
+   *    - Unlike all specializations of std::atomic, it is guaranteed to be lock-free. (Check using is_lock_free())
+   *    - Unlike std::atomic<bool>, std::atomic_flag does not provide load or store operations.
+   */
 
-    /** \example: Checking lock free */
-    std::atomic<int> num2_a{0};
-    std::atomic<float> num2_b{0.0f};
-    std::atomic<int *> num2p;
-    std::atomic<myStruct1> str2_a;
-    // std::atomic<myStruct2> str2_b; // COMPILE ERROR..
+  /** \example: Checking lock free */
+  std::atomic<int> num2_a{0};
+  std::atomic<float> num2_b{0.0f};
+  std::atomic<int *> num2p;
+  std::atomic<myStruct1> str2_a;
+  // std::atomic<myStruct2> str2_b; // COMPILE ERROR..
 
-    std::cout << std::boolalpha << num2_a.is_lock_free() << std::endl;
-    std::cout << std::boolalpha << num2_b.is_lock_free() << std::endl;
-    std::cout << std::boolalpha << num2p.is_lock_free() << std::endl;
-    std::cout << std::boolalpha << str2_a.is_lock_free() << std::endl;
-    // std::cout << std::boolalpha << str2_b.is_lock_free() << std::endl;
+  std::cout << std::boolalpha << num2_a.is_lock_free() << std::endl;
+  std::cout << std::boolalpha << num2_b.is_lock_free() << std::endl;
+  std::cout << std::boolalpha << num2p.is_lock_free() << std::endl;
+  std::cout << std::boolalpha << str2_a.is_lock_free() << std::endl;
+  // std::cout << std::boolalpha << str2_b.is_lock_free() << std::endl;
 
-    std::cout << "------------------------------------- [↑ Example 2-1 ↑] -------------------------------------" << std::endl;
+  std::cout << "------------------------------------- [↑ Example 2-1 ↑] -------------------------------------" << std::endl;
 
-    /** \example: member functions */
-    std::atomic<int> num2_c{0};
-    int x2 = 42;
-    [[maybe_unused]] int y2 = 0;
-    [[maybe_unused]] bool flag2 = 0;
+  /** \example: member functions */
+  std::atomic<int> num2_c{0};
+  int x2 = 42;
+  [[maybe_unused]] int y2 = 0;
+  [[maybe_unused]] bool flag2 = 0;
 
-    num2_c.store(x2);                        // MF for storing
-    std::cout << "num: " << num2_c.load() << std::endl; // n = x; → COMPILE ERROR! (Deleted)
+  num2_c.store(x2);                                   // MF for storing
+  std::cout << "num: " << num2_c.load() << std::endl; // n = x; → COMPILE ERROR! (Deleted)
 
-    x2 = num2_c.load(); // MF for loading
-    std::cout << "x: " << x2 << std::endl;
+  x2 = num2_c.load(); // MF for loading
+  std::cout << "x: " << x2 << std::endl;
 
-    /** \note: Integer type only */
-    num2_c.fetch_add(x2); // MF for adding (Same with 'n += x')
-    std::cout << "num: " << num2_c.load() << std::endl;
+  /** \note: Integer type only */
+  num2_c.fetch_add(x2); // MF for adding (Same with 'n += x')
+  std::cout << "num: " << num2_c.load() << std::endl;
 
-    y2 = num2_c.exchange(x2); // MF for exchanging (y = num2_c, num2_c = x)
-    std::cout << "x: " << x2 << ", y: " << y2 << ", num: " << num2_c.load() << std::endl;
+  y2 = num2_c.exchange(x2); // MF for exchanging (y = num2_c, num2_c = x)
+  std::cout << "x: " << x2 << ", y: " << y2 << ", num: " << num2_c.load() << std::endl;
 
-    flag2 = num2_c.compare_exchange_strong(x2, y2);
-    /** \details:
-     * if (n == x){
-     *   n = y;
-     *   return true;
-     * }
-     * else{
-     *   x = n;
-     *   return false;
-     * }
-     */
-    std::cout << "flag: " << flag2 << ", num: " << num2_c << ", x: " << x2 << ", y: " << y2 << std::endl;
+  flag2 = num2_c.compare_exchange_strong(x2, y2);
+  /** \details:
+   * if (n == x){
+   *   n = y;
+   *   return true;
+   * }
+   * else{
+   *   x = n;
+   *   return false;
+   * }
+   */
+  std::cout << "flag: " << flag2 << ", num: " << num2_c << ", x: " << x2 << ", y: " << y2 << std::endl;
 
-    while (!num2_c.compare_exchange_weak(x2, y2))
-    {
-        std::cout << "Failed" << std::endl;
-    }
-    std::cout << "flag: " << flag2 << ", num: " << num2_c << ", x: " << x2 << ", y: " << y2 << std::endl;
+  while (!num2_c.compare_exchange_weak(x2, y2))
+  {
+    std::cout << "Failed" << std::endl;
+  }
+  std::cout << "flag: " << flag2 << ", num: " << num2_c << ", x: " << x2 << ", y: " << y2 << std::endl;
 
-    std::cout << "------------------------------------- [↑ Example 2-2 ↑] -------------------------------------" << std::endl;
+  std::cout << "------------------------------------- [↑ Example 2-2 ↑] -------------------------------------" << std::endl;
 
-    /* ---------------------------------------------------------------------------------------------------------- */
+  /* ---------------------------------------------------------------------------------------------------------- */
+
+  /**
+   * \brief: [3] Lock free stack algorithm
+   * \details:
+   */
+
+  constexpr std::size_t stack_size = 10;
+  ThreadSafeStack<int, stack_size> stack;
+
+  // Spawn two threads to push and pop from the stack concurrently
+  std::thread push_thread(push_func, std::ref(stack), stack_size);
+  std::thread pop_thread(pop_func, std::ref(stack), stack_size);
+
+  push_thread.join();
+  pop_thread.join();
+
+  // Check if the remaining stack elements are correct
+  assert(stack.get(0).has_value());
+  assert(stack.get(stack_size - 1).has_value());
+  assert(!stack.get(stack_size).has_value());
+
+  std::cout << "------------------------------------- [↑ Example 3 ↑] -------------------------------------" << std::endl;
+
+  /* ---------------------------------------------------------------------------------------------------------- */
+
+  /**
+   * \brief: [4] Memory order
+   * \details:
+   */
+
+  
+
+  std::cout << "------------------------------------- [↑ Example 4 ↑] -------------------------------------" << std::endl;
+
+  /* ---------------------------------------------------------------------------------------------------------- */
 }
