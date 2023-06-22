e15a1a64277f9aa7648686cf2a34779bfc9d7728
Dongjae Won <bigdj2002@naver.com>
2023-06-22 23:29:51 +0900

    Added annotation

--- libs/cpp-thread/include/cthread_atomic.h
+++ libs/cpp-thread/include/cthread_atomic.h
@@ -27,51 +27,56 @@ template <typename T, std::size_t Capacity>
 class LockFreeStack
 {
 public:
-  LockFreeStack() : top_idx(-1) {}
+  LockFreeStack() : top_idx(-1) {} // 스택 생성자: 스택 최상단 위치를 -1로 설정합니다.
 
-  bool push(const T &value)
+  bool push(const T &value) // 값을 스택에 푸시합니다.
   {
     int old_idx, new_idx;
     do
     {
+      // 현재 top_idx를 가져옵니다.
       old_idx = top_idx.load();
       new_idx = old_idx + 1;
-      if (new_idx >= static_cast<int>(Capacity))
-      {
-        return false; // Stack overflow
-      }
-    } while (!top_idx.compare_exchange_weak(old_idx, new_idx));
 
+      if (new_idx >= static_cast<int>(Capacity)) // 스택이 가득 찼는지 확인합니다.
+      {
+        return false; // 스택 오버플로우
+      }
+    } while (!top_idx.compare_exchange_weak(old_idx, new_idx)); // top_idx를 new_idx로 업데이트하려고 시도합니다.
+
+    // 스택의 새로운 top에 값을 넣습니다.
     data[new_idx] = value;
     return true;
   }
 
-  std::optional<T> pop()
+  std::optional<T> pop() // 스택의 최상단 값을 팝합니다.
   {
     int old_idx, new_idx;
     do
     {
       old_idx = top_idx.load();
-      if (old_idx < 0)
+
+      if (old_idx < 0) // 스택이 비어 있는지 확인합니다.
       {
-        return std::nullopt; // Stack underflow
+        return std::nullopt; // 스택 언더플로우
       }
       new_idx = old_idx - 1;
-    } while (!top_idx.compare_exchange_weak(old_idx, new_idx));
 
-    return data[old_idx];
+    } while (!top_idx.compare_exchange_weak(old_idx, new_idx)); // top_idx를 new_idx로 업데이트하려고 시도합니다.
+
+    return data[old_idx]; // 팝할 값을 반환합니다.
   }
 
-  std::optional<T> get(std::size_t index)
+  std::optional<T> get(std::size_t index) // 주어진 인덱스의 스택 값을 가져옵니다.
   {
     if (index >= Capacity || index < 0)
     {
-      return std::nullopt; // Invalid index
+      return std::nullopt; // 유효하지 않은 인덱스
     }
     return data[index];
   }
 
-  void print()
+  void print() // 스택의 모든 값을 출력합니다.
   {
     for (int i = 0; i <= top_idx.load(); ++i)
     {
@@ -81,49 +86,49 @@ public:
   }
 
 private:
-  std::array<T, Capacity> data;
-  std::atomic<int> top_idx;
+  std::array<T, Capacity> data; // 스택의 데이터를 저장하는 배열입니다.
+  std::atomic<int> top_idx;     // 스택의 최상단 위치를 나타내는 원자적 변수입니다.
 };
 
 template <typename T, std::size_t Capacity>
 class LockFreeQueue
 {
 public:
-  LockFreeQueue() : head(0), tail(0) {}
+  LockFreeQueue() : head(0), tail(0) {} // 큐 생성자: 헤드와 테일을 0으로 설정합니다.
 
-  bool push(const T &value)
+  bool push(const T &value) // 값을 큐에 푸시합니다.
   {
     std::size_t currTail = tail.load();
     std::size_t nextTail = (currTail + 1) % Capacity;
 
-    if (nextTail == head.load())
-      return false; // Queue is full
+    if (nextTail == head.load()) // 큐가 가득 찼는지 확인합니다.
+      return false;              // 큐가 가득 찼습니다.
 
     buffer[currTail] = value;
     tail.store(nextTail);
     return true;
   }
 
-  bool pop(T &value)
+  bool pop(T &value) // 큐의 가장 앞쪽 값을 팝합니다.
   {
     std::size_t currHead = head.load();
-    if (currHead == tail.load())
-      return false; // Queue is empty
+    if (currHead == tail.load()) // 큐가 비어 있는지 확인합니다.
+      return false;              // 큐가 비어 있습니다.
 
     value = buffer[currHead];
     head.store((currHead + 1) % Capacity);
     return true;
   }
 
-  std::optional<T> get(std::size_t index)
+  std::optional<T> get(std::size_t index) // 주어진 인덱스의 큐 값을 가져옵니다.
   {
     if (index >= Capacity)
-      return std::nullopt; // Invalid index
+      return std::nullopt; // 유효하지 않은 인덱스
 
     std::size_t currHead = head.load();
     std::size_t currTail = tail.load();
 
-    if (currHead <= currTail)
+    if (currHead <= currTail) // 큐가 비어 있지 않을 때 원하는 위치의 값을 찾습니다.
     {
       if (index < currTail - currHead)
         return buffer[currHead + index];
@@ -138,10 +143,10 @@ public:
         return buffer[wrappedIndex];
     }
 
-    return std::nullopt; // Index out of range
+    return std::nullopt; // 인덱스가 범위를 벗어났습니다.
   }
 
-  void printQueue()
+  void printQueue() // 큐의 모든 값을 출력합니다.
   {
     std::size_t currentHead = head.load(std::memory_order_relaxed);
     std::size_t currentTail = tail.load(std::memory_order_relaxed);
@@ -152,17 +157,16 @@ public:
     }
   }
 
-  bool IsEmpty() const
+  bool IsEmpty() const // 큐가 비어 있는지 확인합니다.
   {
     return head.load() == tail.load();
   }
 
 private:
-  std::array<T, Capacity> buffer;
-  std::atomic<std::size_t> head;
-  std::atomic<std::size_t> tail;
-
-  std::size_t IncrementIndex(std::size_t index) const
+  std::array<T, Capacity> buffer;                     // 큐의 데이터를 저장하는 배열입니다.
+  std::atomic<std::size_t> head;                      // 큐의 헤드 위치를 나타내는 원자적 변수입니다.
+  std::atomic<std::size_t> tail;                      // 큐의 테일 위치를 나타내는 원자적 변수입니다.
+  std::size_t IncrementIndex(std::size_t index) const // 인덱스를 증가시키는 메서드입니다.
   {
     return (index + 1) % Capacity;
   }
