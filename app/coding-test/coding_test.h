fd065bc89dae9b01c649f502fd065abc5b4643bc
harvey <harvey@mediaexcel.com>
2023-12-16 02:15:25 +0000

    leedcode 88

--- app/coding-test/coding_test.h
+++ app/coding-test/coding_test.h
@@ -1,5 +1,6 @@
 #include "q_0001.h"
 #include "q_0002.h"
+#include "q_0088.h"
 #include "q_0945.h"
 
 #include <iostream>
---
+++ libs/leet-code/source/q_0088.cpp
@@ -0,0 +1,20 @@
+#include "q_0088.h"
+
+void leet_test_0088()
+{
+    Solution_0088 sol;
+
+    int m = 3;
+    std::vector<int> nums1 = {1, 2, 3, 0, 0, 0};
+
+    int n = 3;
+    std::vector<int> nums2 = {2, 5, 6};
+
+    sol.merge(nums1, m, nums2, n);
+
+    for (unsigned i = 0; i < nums1.size(); ++i)
+    {
+        printf("%d ", nums1[i]);
+    }
+    printf("\n");
+}
